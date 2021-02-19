d37c87d6e3216c8722c06577eae8b092443a6e77
Mingkun He <hmingku@amazon.com>
2021-02-18 16:35:13 -0800

    add region partition test

--- integration/helpers/base_test.py
+++ integration/helpers/base_test.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from integration.helpers.client_provider import ClientProvider
-from integration.helpers.resource import generate_suffix, create_bucket, verify_stack_resources
+from integration.helpers.resource import generate_suffix, create_bucket, verify_stack_resources, load_yaml
 
 try:
     from pathlib import Path
@@ -305,12 +305,12 @@ class BaseTest(TestCase):
         value
             value
         """
-        yaml_doc = self._load_yaml(self.sub_input_file_path)
+        yaml_doc = load_yaml(self.sub_input_file_path)
         yaml_doc["Resources"][resource_name]["Properties"][property_name] = value
         self._dump_yaml(self.sub_input_file_path, yaml_doc)
 
     def get_template_resource_property(self, resource_name, property_name):
-        yaml_doc = self._load_yaml(self.sub_input_file_path)
+        yaml_doc = load_yaml(self.sub_input_file_path)
         return yaml_doc["Resources"][resource_name]["Properties"][property_name]
 
     def deploy_stack(self, parameters=None):
@@ -342,24 +342,6 @@ class BaseTest(TestCase):
         self.assertEqual(self.stack_description["Stacks"][0]["StackStatus"], "CREATE_COMPLETE")
         # verify if the stack contains the expected resources
         self.assertTrue(verify_stack_resources(self.expected_resource_path, self.stack_resources))
-
-    def _load_yaml(self, file_path):
-        """
-        Loads a yaml file
-
-        Parameters
-        ----------
-        file_path : Path
-            File path
-
-        Returns
-        -------
-        Object
-            Yaml object
-        """
-        with open(file_path) as f:
-            data = f.read()
-        return yaml.load(data, Loader=yaml.FullLoader)
 
     def _dump_yaml(self, file_path, yaml_doc):
         """
